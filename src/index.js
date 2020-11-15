6cd95a151153cee23790fb47bbf9597039891ac9
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-15 19:00:34 +0000

    add provider and store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import store from './store.js'
 
 ReactDOM.render(
-  <React.StrictMode>
+  // store is prop that must be provided and store must be created using createStore
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
