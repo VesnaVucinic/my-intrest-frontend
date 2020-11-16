034f60814fe1d0e0fc3354706e7b347f5ff918f2
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-16 22:59:48 +0000

    import Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+import Login from './components/Login.js'
+
 
 class App extends React.Component {
 
   render() {
     return (
-      "Hello I'm React"
+      <Login/>
     );
   }
 }
