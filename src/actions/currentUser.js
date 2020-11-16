4efea88a6a838f5ef2b60dc6b504fe795064b813
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-16 23:02:35 +0000

    add setCurrentUser

---
+++ src/actions/currentUser.js
@@ -0,0 +1,20 @@
+// synchronous action creators
+export const setCurrentUser = user => {
+    return {
+      type: "SET_CURRENT_USER",
+      user
+    }
+}
+
+// asynchronous action creators
+export const login = credentials => {
+    return dispatch => {
+      return fetch("http://localhost:3001/api/v1/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email: "vesna@vesna.com", password: "vv"})
+      })
+    }
+}
