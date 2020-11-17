4071015624af6357ec481b8468be38a73ef9ebfc
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-17 00:21:49 +0000

    add action creator login

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -8,13 +8,23 @@ export const setCurrentUser = user => {
 
 // asynchronous action creators
 export const login = credentials => {
+    console.log(credentials)
     return dispatch => {
-      return fetch("http://localhost:3001/api/v1/login", {
+      return fetch("http://127.0.0.1:3001/api/v1/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({email: "vesna@vesna.com", password: "vv"})
+        body: JSON.stringify(credentials)
       })
+        .then(response =>response.json())
+        .then(response =>{
+            if(response.error) {
+                alert(response.error)
+            } else {
+                dispatch(setCurrentUser(response.data)) 
+            }
+        })
+        .catch(console.log)
     }
 }
