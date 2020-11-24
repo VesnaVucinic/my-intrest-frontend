a9046fa1470566734ae3de9fd045360e27d1bcb0
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 20:47:10 +0000

    add react bootstrap in loggedin Navbar

---
+++ src/components/MyLoggedinNavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Button from 'react-bootstrap/Button'
+import {Link} from 'react-router-dom'
+
+
+
+
+export const MyLoggedInNavBar = ({logOut}) =>{
+    return(
+      <Navbar bg={"dark"}>
+        <div className="container">
+           <Nav> 
+           <Nav.Link as={Link}   style={{color: "azure"}} to="/"></Nav.Link> 
+           <Button size="sm" onClick = {logOut}>Log Out</Button>
+           </Nav>
+        </div>
+    </Navbar>
+
+    )
+}
+
+export default MyLoggedInNavBar
