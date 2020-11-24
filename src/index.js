29f453411295eec48155934c3af9bf396ec264b4
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 21:18:39 +0000

    add provider

--- src/index.js
+++ src/index.js
@@ -2,19 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'
-import store from './store.js'
+import {createStore, applyMiddleware,compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {combineReducers} from 'redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import userReducer from './reducers/userReducer'
+import loadingReducer from './reducers/loadingReducer';
+
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    loading: loadingReducer,
+  })
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+
 
 ReactDOM.render(
-  // store is prop that must be provided and store must be created using createStore
-  <Provider store={ store }>
+<Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+</Provider>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
