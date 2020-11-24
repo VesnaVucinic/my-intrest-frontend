20e076b4e9aa231a600587f75402e7786aef8dd9
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 20:44:59 +0000

    add header stateless component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+
+
+
+export const Header = () =>{
+    return(
+      <Jumbotron >
+        <div className="container title">
+          <h1 className="display-5">Welcome to My interest!</h1>
+        </div>
+    </Jumbotron>
+
+    )
+}
+
+export default Header
