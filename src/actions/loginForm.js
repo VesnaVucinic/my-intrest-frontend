a18255057b5d88cff4460a5fc99bae857cbf3951
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 21:19:00 +0000

    deleted files

--- src/actions/loginForm.js
+++
@@ -1,7 +0,0 @@
-export const updateLoginForm = (formData) => {
-    console.log(formData)
-    return {
-      type: "UPDATE_LOGIN_FORM",
-      formData
-    }
-}
--- src/reducers/currentUser.js
+++
@@ -1,9 +0,0 @@
-export default (state = null, action) => {
-    // null instead empty becouse to use for trouthiness check
-    switch (action.type) {
-      case "SET_CURRENT_USER":
-        return action.user
-      default:
-        return state
-    }
-}
