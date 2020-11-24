17be7f43d91138a3aad0a3ef0856f5a71f3b4f93
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-24 20:52:20 +0000

    add link and navBar

---
+++ src/components/MyNotLoggedinNavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import {Link} from 'react-router-dom'
+
+
+
+
+export const MyNotLoggedInNavBar = () =>{
+    return(
+      <Navbar bg={"dark"}>
+        <div className="container">
+           <Nav>
+           <Nav.Link as={Link}  style={{color: "azure"}} to="/"></Nav.Link>
+           <Nav.Link as={Link}  style={{color: "azure"}} to="/signup">Sign up</Nav.Link>
+           <Nav.Link as={Link}  style={{color: "azure"}} to="/login">Log in</Nav.Link>
+           </Nav>
+        </div>
+    </Navbar>
+
+    )
+}
+
+export default MyNotLoggedInNavBar
