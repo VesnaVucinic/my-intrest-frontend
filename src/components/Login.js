bcd4be373b5cbb9f94d87dfd11a952e68949dab8
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-28 23:31:24 +0000

    add semantic-ui-react form

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { updateLoginForm } from "../actions/loginForm.js"
 import { login } from "../actions/currentUser.js"
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+// import Form from 'react-bootstrap/Form'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 const Login = ({ loginFormData, updateLoginForm, login, history }) => {
     // instead ({email, password}) can be (props) but in that case is value={props.email} value={props.password}
@@ -21,29 +23,67 @@ const Login = ({ loginFormData, updateLoginForm, login, history }) => {
         login(loginFormData, history)
     }
 
-    return (        
-      <Form className="container" onSubmit={handleSubmit} >
-          <h1>Log In Form</h1>
-          <Form.Group controlId="formUserName">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control 
-                 name = "email"
-                 onChange={handleInputChange} 
-                 type="email" 
-                 value={loginFormData.email}/>
-          </Form.Group>
+  //   return (        
+  //     <Form className="container" onSubmit={handleSubmit} >
+  //         <h1>Log In Form</h1>
+  //         <Form.Group controlId="formUserName">
+  //             <Form.Label>Email address</Form.Label>
+  //             <Form.Control 
+  //                name = "email"
+  //                onChange={handleInputChange} 
+  //                type="email" 
+  //                value={loginFormData.email}/>
+  //         </Form.Group>
          
-          <Form.Group controlId="formUserName">
-               <Form.Label>Password</Form.Label>
-               <Form.Control 
-                  name = "password"
-                  onChange={handleInputChange} 
-                  type="password" 
-                  value={loginFormData.password}/>
-          </Form.Group>
+  //         <Form.Group controlId="formUserName">
+  //              <Form.Label>Password</Form.Label>
+  //              <Form.Control 
+  //                 name = "password"
+  //                 onChange={handleInputChange} 
+  //                 type="password" 
+  //                 value={loginFormData.password}/>
+  //         </Form.Group>
 
-          <Button variant="primary" type="submit">Log in</Button>
-      </Form>
+  //         <Button variant="primary" type="submit">Log in</Button>
+  //     </Form>
+  // )
+
+  return (
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+      {/* { (error != '')
+      ?
+      <Message negative><p>Username/Password is Incorrect</p></Message>
+      :
+      null
+      } */}
+        <Header as='h2' color='black' textAlign='center'>
+          Log-in to your account
+      </Header>
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input fluid icon='user' iconPosition='left' name = "email" placeholder='E-mail address' type="email" onChange={handleInputChange}  />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              name = "password"
+              placeholder='Password'
+              type='password'
+              onChange={handleInputChange} 
+              
+            />
+
+            <Button color='black' fluid size='large' onClick={handleSubmit}>
+              Login
+          </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href='signup'>Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
   )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { updateSignupForm } from "../actions/signupForm.js"
 import { signup } from "../actions/currentUser.js"
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/Button'
+// import Form from 'react-bootstrap/Form'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 const Signup = ({ signupFormData, updateSignupForm, signup, history }) => {
     // instead ({email, password}) can be (props) but in that case is value={props.email} value={props.password}
@@ -21,36 +23,69 @@ const Signup = ({ signupFormData, updateSignupForm, signup, history }) => {
         signup(signupFormData, history)
     }
 
-    return (        
-        <Form className="container" onSubmit={handleSubmit} >
-            <h1>Sign Up Form</h1>
-            <Form.Group controlId="formUserName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control 
-                   name = "name"
-                   onChange={handleInputChange} 
-                   type="name" 
-                   value={signupFormData.name}/>
-            </Form.Group>
-            <Form.Group controlId="formUserName">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control 
-                   name = "email"
-                   onChange={handleInputChange} 
-                   type="email" 
-                   value={signupFormData.email}/>
-            </Form.Group>
-            <Form.Group controlId="formUserName">
-                 <Form.Label>Password</Form.Label>
-                 <Form.Control 
-                    name = "password"
-                    onChange={handleInputChange} 
-                    type="password" 
-                    value={signupFormData.password}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">Sign Up</Button>
-        </Form>
-    )
+    // return (        
+    //     <Form className="container" onSubmit={handleSubmit} >
+    //         <h1>Sign Up Form</h1>
+    //         <Form.Group controlId="formUserName">
+    //             <Form.Label>Name</Form.Label>
+    //             <Form.Control 
+    //                name = "name"
+    //                onChange={handleInputChange} 
+    //                type="name" 
+    //                value={signupFormData.name}/>
+    //         </Form.Group>
+    //         <Form.Group controlId="formUserName">
+    //             <Form.Label>Email address</Form.Label>
+    //             <Form.Control 
+    //                name = "email"
+    //                onChange={handleInputChange} 
+    //                type="email" 
+    //                value={signupFormData.email}/>
+    //         </Form.Group>
+    //         <Form.Group controlId="formUserName">
+    //              <Form.Label>Password</Form.Label>
+    //              <Form.Control 
+    //                 name = "password"
+    //                 onChange={handleInputChange} 
+    //                 type="password" 
+    //                 value={signupFormData.password}/>
+    //         </Form.Group>
+    //         <Button variant="primary" type="submit">Sign Up</Button>
+    //     </Form>
+    // )
+    return (
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='black' textAlign='center'>
+              Sign-up to create your account
+          </Header>
+            <Form size='large'>
+              <Segment stacked>
+                <Form.Input fluid icon='user' iconPosition='left' name = "name" placeholder='Name' type="name" onChange={handleInputChange}  />
+                <Form.Input fluid icon='user' iconPosition='left' name = "email" placeholder='E-mail address' type="email" onChange={handleInputChange}  />
+                <Form.Input
+                  fluid
+                  icon='lock'
+                  iconPosition='left'
+                  name = "password"
+                  placeholder='Password'
+                  type='password'
+                  onChange={handleInputChange} 
+                  
+                />
+    
+                <Button color='black' fluid size='large' onClick={handleSubmit}>
+                  Sign Up
+              </Button>
+              </Segment>
+            </Form>
+            <Message>
+              Already have account? <a href='login'>Log In</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      )
+
 }
 
 const mapStateToProps = state => {
