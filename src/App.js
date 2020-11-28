6e0375be55d85df4de97852f2011c330ebf716c4
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-28 10:37:53 +0000

    add login and signup routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import { getCurrentUser } from "./actions/currentUser.js"
 import { connect } from 'react-redux';
 import NavBar from './components/NavBar.js'
+import Login from './components/Login.js'
+import Signup from './components/Signup.js'
+import Logout from './components/Logout.js'
 import MainContainer from './components/MainContainer.js';
+import { Route } from 'react-router-dom'
 
 class App extends React.Component {
   // whenever component mount I am sending request to check is someone is logged in
@@ -14,10 +18,16 @@ class App extends React.Component {
 
   render() {
     return (
+   
      <div className="App">
+      <Logout/>
       <NavBar/>
-      <MainContainer/>
+      {/* <MainContainer/> */} 
+          <Route exact path='/signup' component={Signup}/>
+          <Route exact path='/login' component={Login}/>
+          {/* <Route exact path='/logout' component={Logout}/> */}
      </div>
+     
     );
   }
 }
