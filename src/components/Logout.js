6d396b1eba654ed066e0ec585e3e666b1c6e062c
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-11-30 00:24:49 +0000

    add log out button

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -6,9 +6,29 @@ import Button from 'react-bootstrap/Button'
 
 const Logout = ({ logout }) => {
   return (
-    <Button size="sm" onClick = { logout }>Log Out</Button>
-  )
+    <Button variant="light" className="justify-content-end" size="sm" onClick = { logout }>Log Out</Button>
+    // <Container>
+    //   <Button size='small' onClick = { logout }>Log Out</Button>
+    // </Container>
+    )
 }
+
+
+// import { Menu, Container } from 'semantic-ui-react'
+// import { NavLink, withRouter } from 'react-router-dom'
+// import { Button } from 'semantic-ui-react'
+
+// const Logout = ({ logout }) => {
+//   return (
+//     // <Button size="sm" onClick = { logout }>Log Out</Button>
+//     <Menu.Item
+//             as={NavLink} to="/logout"
+//             name='Log Out'
+//             active={activeItem === 'logout'}
+//             onClick={ logout }
+//     />
+//   )
+// }
 
 export default connect(null, { logout } )(Logout)
 
