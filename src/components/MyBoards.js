243d7d2323ffc376c712fd8e93cb2d81c9e48b2a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-03 20:50:07 +0000

    add list of boards

---
+++ src/components/MyBoards.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+// const MyBoards = props => {
+//     const boardCards = props.boards.length > 0 ? 
+//         props.boards.map(b => (<p key={b.id}><Link to={`/boards/${b.id}`}>{b.atributes.name}</Link></p>)) :
+//         null
+
+// return boardCards
+// }       
+
+// const mapStateToProps = state => {
+//     return {
+//         boards: state.MyBoards
+//     }
+// }
+
+// export default connect(mapStateToProps)(MyBoards)
+
+const MyBoards = ()  => {
+    const boardCards = "Hello"
+
+return boardCards
+}       
+
+
+
+export default MyBoards
