1d224a5ab8fdc7e3b2aa3a4e12d8230f9ecd5e50
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-03 20:51:40 +0000

    add case SET_MY_BOARDS

---
+++ src/reducers/myBoards.js
@@ -0,0 +1,10 @@
+const initialState =[]
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case "SET_MY_BOARDS":
+        return action.boards
+        default:
+            return state   
+    }
+}
