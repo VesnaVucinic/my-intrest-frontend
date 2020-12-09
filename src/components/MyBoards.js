4482f5af28d1253209b4daec4c12016aac77ee7c
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-09 18:38:41 +0000

    add map through boards

--- src/components/MyBoards.js
+++ src/components/MyBoards.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import BoardCard from './BoardCard.js'
+
+const MyBoards = props => {
+    const boardCards = props.boards.length > 0  ? 
+    props.boards.map(board => <BoardCard board={board} key={board.id}  />) :
+    null 
+    return (
+        boardCards 
+    )
+}
 
 // const MyBoards = props => {
 //     const boardCards = props.boards.length > 0 ? 
 //         props.boards.map(b => (<p key={b.id}><Link to={`/boards/${b.id}`}>{b.atributes.name}</Link></p>)) :
 //         null
+   
+//     return boardCards
+// } 
 
-// return boardCards
-// }       
 
-// const mapStateToProps = state => {
-//     return {
-//         boards: state.MyBoards
-//     }
-// }
+const mapStateToProps = state => {
+    return {
+        boards: state.myBoards
+    }
+}
 
-// export default connect(mapStateToProps)(MyBoards)
-
-const MyBoards = ()  => {
-    const boardCards = "Hello"
-
-return boardCards
-}       
-
-export default MyBoards
+export default connect(mapStateToProps)(MyBoards)
