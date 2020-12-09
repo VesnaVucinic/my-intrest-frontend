fc61f34cfdffa2decf20a8e477563a10deed6a93
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-09 18:29:55 +0000

    add case UPDATE_NEW_BOARD_FORM and RESET_NEW_BOARD_FORM

---
+++ src/reducers/newBoardForm.js
@@ -0,0 +1,17 @@
+const initialState = {
+    name: ""
+  }
+  
+  export default (state=initialState, action) => {
+    switch (action.type) {
+      case "UPDATE_NEW_BOARD_FORM":
+       return {
+         ...state,
+         [action.formData.name]: action.formData.value
+       }
+      case "RESET_NEW_BOARD_FORM":
+        return initialState
+      default:
+        return state
+    }
+}
