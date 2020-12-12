df438fca56f465589b3d239748342c41adbb6063
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-12 23:29:33 +0000

    add handleSubmit and handleChange, mstp and mdtp as obj syntax

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,35 +1,75 @@
 import React from 'react';
+// 1. We first grabthe action creator 
 import { updateNewBoardForm } from "../actions/newBoardForm.js"
+import { createBoard } from  '../actions/myBoards'
 import { connect } from 'react-redux'
+import Container from 'react-bootstrap/Container'
 
-const NewBoardForm = ({ name, history }) => {
-   const handleChange= event =>{
+// 3. This means redux gives us back a prop called updateBoardForm 
+//  which when invocked, actually Redux will now dispatch 
+const NewBoardForm = ({ formData, history, updateNewBoardForm, createBoard, userId }) => {
+   
+   const{ name, imageUrl } = formData
+
+    const handleChange= event =>{
+    console.log("trigger Handle change")
        const { name,value } = event.target
+    // 4. this is not an invocation of just the action creator,
+    // it's now Redux dispatching the action  built by the actions
+    // creator with apropriate arguments 
        updateNewBoardForm(name, value)
+       
    }
 
-    const handleSubmit = event => event.preventDefault()
+    const handleSubmit = event => {
+        event.preventDefault()
+        createBoard({
+            ...formData,
+            userId
+        })
+
+        // formData: {
+        //     name: ""
+        //     imageUrl: ""
+        // }
+
+    }
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                name="name"
-                placeholder="name"
-                onChange={handleChange}
-                value={name}
-            />
-            <input 
-                type="submit" 
-                value="Create Board"
-            />
-        </form>
-    )
+        <Container >
+            <div className="NewBoardForm">
+            <form onSubmit={handleSubmit}>
+             {/* event.preventDefault() */}
+                <input
+                    name="name"
+                    placeholder="name"
+                    onChange={handleChange}
+                    value={name}
+                /><br/>
+                <input
+                    name="imageUrl"
+                    placeholder="imageUrl"
+                    onChange={handleChange}
+                    value={imageUrl}
+                /><br/>
+                <input 
+                    type="submit" 
+                    value="Create Board"
+                />
+            </form>
+            </div>
+            </Container>    
+    )   
     };
 const mapStateToProps = state => {
-    const { name } = state.newBoardForm
+    // const { name, imageUrl } = state.newBoardForm
+    // const userId = state.currentUser ? state.currentUser.id : ""
     return {
-        name
-        // formData: state.newBoardForm
+        // name,
+        // imageUrl
+        formData: state.newBoardForm,
+        userId: state.currentUser ? state.currentUser.id : ""
     }
 }
-
-export default connect(mapStateToProps, { updateNewBoardForm })(NewBoardForm);
+// 2. We pass the action creator to redux's connect function
+// using eather mapDespatchToProps or the shorthand object syntax seen below
+export default connect(mapStateToProps, { updateNewBoardForm, createBoard })(NewBoardForm);
