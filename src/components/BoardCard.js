94426c8a7c7ec26e5830e622ac8ad896ae0c3c4e
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-12 23:33:37 +0000

    add image url

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
 
 const BoardCard = ({ board }) => {
     return (
-        <div>
-            <h4>{board.attributes.name}</h4>
-            <img src={board.attributes.image_url} hight="100" width="100"  alt="..."></img>
-        </div>
+        <Container>
+            {/* <h4>{board.attributes.name}</h4> */}
+                <img src={board.attributes.image_url} hight="150" width="150"  alt="..."></img>
+        </Container>
     )
 }
 
