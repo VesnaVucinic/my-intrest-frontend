16a28527eabfaab9cc265bb4c875811ff140ab39
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-14 22:52:33 +0000

    add createBoard and updateBoard

--- src/actions/myBoards.js
+++ src/actions/myBoards.js
@@ -1,4 +1,4 @@
-
+import { resetBoardForm } from './boardForm'
 // synchronous actions
 export const setMyBoards = boards => {
     return {
@@ -17,6 +17,13 @@ export const clearBoards = () => {
 export const addBoard = board => {
     return {
         type: "ADD_BOARD",
+        board
+    }
+}
+
+export const updateBoardSuccess = board => {
+    return {
+        type: "UPDATE_BOARD",
         board
     }
 }
@@ -52,9 +59,7 @@ export const getMyBoards = () => {
 } 
 
 
-
-
-export const createBoard = boardData => {
+export const createBoard = (boardData, history) => {
     return dispatch => {
         const token = localStorage.token
         const sendableBoardData = {
@@ -71,7 +76,53 @@ export const createBoard = boardData => {
             body: JSON.stringify(sendableBoardData)
         }) 
         .then(r => r.json())
-        .then(console.log)
+        .then(resp => {
+            if (resp.error) {
+                alert(resp.error)
+            } else {
+            dispatch(addBoard(resp.data))
+            // add new trip to store: dispatch action by invocing that action creator with the information I need sddBoard 
+            // that returns to me object I actualy despatching which will then trriger or invoke
+            // all of my reducers the one I am gonna catch on is case "ADD_BOARD" where I will concarenate that new board to my state 
+            dispatch(resetBoardForm())
+            history.push(`/boards/${resp.data.id}`)
+        }
+        })
+
+        .catch(console.log)
+    }
+}
+
+export const updateBoard = (boardData,history) => {
+    return dispatch => {
+        const token = localStorage.token
+        const sendableBoardData = {
+            name: boardData.name,
+            image_url: boardData.imageUrl,
+            user_id: boardData.userId
+        }        
+        return fetch(`http://127.0.0.1:3001/api/v1/boards/${boardData.boardId}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+              'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(sendableBoardData)
+        }) 
+        .then(r => r.json())
+        .then(resp => {
+            if (resp.error) {
+                alert(resp.error)
+            } else {
+            dispatch(updateBoardSuccess(resp.data))
+            // add new trip to store: dispatch action by invocing that action creator with the information I need sddBoard 
+            // that returns to me object I actualy despatching which will then trriger or invoke
+            // all of my reducers the one I am gonna catch on is case "ADD_BOARD" where I will concarenate that new board to my state 
+            dispatch(resetBoardForm())
+            history.push(`/boards/${resp.data.id}`)
+        }
+        })
+
         .catch(console.log)
     }
 }
