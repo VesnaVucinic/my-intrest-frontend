3fe7c3434eef29637040c2434f4e4e9abe338624
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-14 22:54:40 +0000

    add link to edit board

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
+import { Link } from 'react-router-dom'
 
 const BoardCard = ({ board }) => {
     return (
+        board ?
+        <div>
         <Container>
-            {/* <h4>{board.attributes.name}</h4> */}
-                <img src={board.attributes.image_url} hight="150" width="150"  alt="..."></img>
+            <h4>{board.attributes.name}</h4>
+            <img src={board.attributes.image_url} hight="150" width="150"  alt="..."></img><br/><br/>
+            <Link to={`/boards/${board.id}/edit`}>Edit this board</Link>
         </Container>
+        </div>:
+        <p>This the the Trip card with no trip!</p>
+        
     )
 }
 
