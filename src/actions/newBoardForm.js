a047a978aeb703f825682fb8afe9ea994524625d
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-14 22:53:27 +0000

    add const addBoard

--- src/actions/newBoardForm.js
+++
@@ -1,9 +0,0 @@
-// sync actions
-export const updateNewBoardForm = (name, value) => {
-    const formData = { name, value }
-    console.log("formData in action creator", formData)
-    return {
-        type: "UPDATE_NEW_BOARD_FORM",
-        formData: { name, value }
-    }
-}
