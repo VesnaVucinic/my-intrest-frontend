56008e2ee3e8065dc8d2d0f92024ce84433f811a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-14 23:02:13 +0000

    add case ADD_BOARD

--- src/reducers/myBoards.js
+++ src/reducers/myBoards.js
@@ -4,6 +4,11 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case "SET_MY_BOARDS":
             return action.boards
+        case "ADD_BOARD":
+            return state.concat(action.board)
+        case "UPDATE_BOARD":
+            console.log("in UPDATE BOARD action is", action)
+            return state
         case "CLEAR_BOARDS":
             return initialState
         default:
