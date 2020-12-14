a95c7e520ec5f1b3f2913b782ca595c23190fe1a
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-14 22:56:13 +0000

    add link to show page

--- src/components/MyBoards.js
+++ src/components/MyBoards.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import BoardCard from './BoardCard.js'
+import { Link } from 'react-router-dom'
+
 
 const MyBoards = props => {
     const boardCards = props.boards.length > 0  ? 
-    props.boards.map(board => <BoardCard board={board} key={board.id}  />) :
+    props.boards.map(board => (<p key={board.id}><Link to={`/boards/${board.id}`}><img src={board.attributes.image_url} hight="150" width="150"  alt="..."></img></Link></p>)) : 
     null 
     return (
         boardCards 
