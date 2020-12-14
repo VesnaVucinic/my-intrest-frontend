5c5cf60244353a2fa03baed288efd0162a87076d
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-14 23:11:00 +0000

    add case SET_FORM_DATA_FOR_EDIT

---
+++ src/reducers/boardForm.js
@@ -0,0 +1,23 @@
+const initialState = {
+    name: "",
+    imageUrl: ""
+  }
+  
+  export default (state=initialState, action) => {
+    // console.log("incoming action is", action)
+    switch (action.type) {
+      case "UPDATE_BOARD_FORM": 
+       const returnVal = {
+         ...state,
+         [action.formData.name]: action.formData.value
+       }
+      //  console.log("retrurnValue is", returnVal)
+       return returnVal
+      case "RESET_BOARD_FORM":
+        return initialState
+      case "SET_FORM_DATA_FOR_EDIT":
+        return action.boardFormData
+      default:
+        return state
+    }
+}
