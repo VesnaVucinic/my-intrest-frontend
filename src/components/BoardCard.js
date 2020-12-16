b99cb4ad4b1c819de98a119149674bc1db8468a5
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-16 19:10:01 +0000

    add delete button

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import { Link } from 'react-router-dom'
+import { deleteBoard  } from  '../actions/myBoards'
+import { connect } from 'react-redux'
+import Button from 'react-bootstrap/Button'
 
-const BoardCard = ({ board }) => {
+
+
+
+const BoardCard = ({ board  }) => {
+    // const { history, deleteBoard } = props
+    // const boardId = board ? board.id : null
     return (
         board ?
         <div>
@@ -10,12 +18,13 @@ const BoardCard = ({ board }) => {
             <h4>{board.attributes.name}</h4>
             <img src={board.attributes.image_url} hight="150" width="150"  alt="..."></img><br/><br/>
             <Link to={`/boards/${board.id}/edit`}>Edit this board</Link>
+            {/* <Button variant="light" className="justify-content-cener" size="sm" onClick={()=>deleteBoard(boardId, history)}>Delete this board</Button> */}
         </Container>
         </div>:
-        <p>This the the Trip card with no trip!</p>
+        <p>This the the Board Card with no board!</p>
         
     )
 }
 
+// export default connect(null, { deleteBoard }(BoardCard))
 export default BoardCard
-
