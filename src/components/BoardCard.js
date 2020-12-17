cc15bc738088e4617cc4f22915fc4a15fb698015
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-17 18:23:20 +0000

    add delete functionality

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -8,17 +8,16 @@ import Button from 'react-bootstrap/Button'
 
 
 
-const BoardCard = ({ board  }) => {
-    // const { history, deleteBoard } = props
-    // const boardId = board ? board.id : null
+const BoardCard = ({ board, history, deleteBoard }) => {
+    const boardId = board ? board.id : null
     return (
         board ?
         <div>
         <Container>
             <h4>{board.attributes.name}</h4>
             <img src={board.attributes.image_url} hight="150" width="150"  alt="..."></img><br/><br/>
-            <Link to={`/boards/${board.id}/edit`}>Edit this board</Link>
-            {/* <Button variant="light" className="justify-content-cener" size="sm" onClick={()=>deleteBoard(boardId, history)}>Delete this board</Button> */}
+            <Link to={`/boards/${board.id}/edit`}>Edit this board</Link><br/>
+            <Button variant="light" className="justify-content-cener" size="sm" onClick={()=>deleteBoard(boardId, history)}>Delete this board</Button>
         </Container>
         </div>:
         <p>This the the Board Card with no board!</p>
@@ -26,5 +25,4 @@ const BoardCard = ({ board  }) => {
     )
 }
 
-// export default connect(null, { deleteBoard }(BoardCard))
-export default BoardCard
+export default connect(null, { deleteBoard })(BoardCard)
