a18266d07348e913f8e32a6420f953ea4c5d572d
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-18 18:59:48 +0000

    add case SET_All_BOARDS

--- src/reducers/allBoards.js
+++ src/reducers/allBoards.js
@@ -4,7 +4,7 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case "SET_All_BOARDS":
             return action.boards
-        case "CLEAR_BOARDS":
+        case "CLEAR_ALL_BOARDS":
             return initialState
         default:
             return state   
