199ec64bf1d7607d11cd8f081132a5518d18ebd3
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-18 19:07:12 +0000

    add route for all-boards

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Header from './components/Header'
 import Login from './components/Login.js'
 import Signup from './components/Signup.js'
 import MyBoards from './components/MyBoards.js'
+import AllBoards from './components/AllBoards.js'
+
 import BoardForm from './components/BoardForm'
 import BoardCard from './components/BoardCard'
 import Home from './components/Home'
@@ -21,6 +23,7 @@ import { Route, Switch, withRouter, Link } from 'react-router-dom'
 import MainContainer from './components/MainContainer';
 import Logout from './components/Logout';
 import EditBoardFormWrapper from './components/EditBoardFormWrapper';
+import allBoards from './reducers/allBoards';
 
 
 class App extends React.Component {
@@ -36,17 +39,19 @@ class App extends React.Component {
       <div className="App">
         {/* { loggedIn ? <NavBar/> : <Home/> } */}
         {loggedIn ? <LoggedInNavBar/> : <NotLoggedInNavBar/>}
+        
         {/* <Header /> */}
         <Switch>
           <Route exact path='/signup' render={()=><Signup/>}/>
           <Route exact path='/login' component={Login}/>
           {/* <Route exact path='/' render={() => loggedIn ? <MyBoards/> : <Home/>}/> */}
           <Route exact path='/boards' component={MyBoards}/>
+          
           <Route exact path='/boards/new' component={NewBoardFormWrapper}/>
-        {/* I am rendering new board as a child directly of a route and when I use component then route will authomaticly pass those props along to this component 
-        if I split it up into render like for Signup then I have to be more specific which roure props I want to supply
-        like: <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
-        ?then I dont need withRouter*/}
+          {/* I am rendering new board as a child directly of a route and when I use component then route will authomaticly pass those props along to this component 
+          if I split it up into render like for Signup then I have to be more specific which roure props I want to supply
+          like: <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
+          ?then I dont need withRouter*/}
           <Route exact path='/boards/:id' render={props => {
           //  I need to pass dowm attributes of board and obj that contains that info is my trip obj which I need to get from state 
           // router props that givs me ability to look at the parametars from the URL and grab the one I need and that is match: match.params.id
@@ -57,13 +62,21 @@ class App extends React.Component {
           // obj that I found I am passing down as a prop to my board card board={board}
           return <BoardCard board={board} {...props}/>
           }
-        }/>
+          }/>
           <Route exact path='/boards/:id/edit' render={props => {
             const board = myBoards.find(board => board.id === props.match.params.id) 
             // dispatch updateForn -> trip, react will not alowed change of state inside render method that need to be pure
             return <EditBoardFormWrapper board={board} {...props}/>
           }
-        }/>
+          }/>
+          <Route exact path='/all-boards' component={AllBoards}/>
+          {/* <Route exact path='/all-boards/:id' render={props => {
+           const board = allBoards.find(board => board.id === props.match.params.id) 
+           console.log(board)
+
+           return <BoardCard board={board} {...props}/>
+          } 
+          }/> */}
         </Switch>
       </div>
           
@@ -75,7 +88,8 @@ const mapStateToProps = state => {
   return ({
     loggedIn: !!state.currentUser,
     // allBoards: state.allBoards
-    myBoards: state.myBoards
+    myBoards: state.myBoards,
+    allBoards: state.allBoards
 
 
     // loogdIn is boolean version of state.currentUser, I dont need whole object currentUser but only to know in app is someone logged in. Can be only currentUser in return
