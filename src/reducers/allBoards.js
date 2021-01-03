7b6dacf2366962084f97acb277c9cd8cdf884a32
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-01-03 00:02:20 +0000

    add cases for likes, delete and update board

--- src/reducers/allBoards.js
+++ src/reducers/allBoards.js
@@ -6,10 +6,16 @@ export default (state = initialState, action) => {
             return action.boards
         case "ADD_BOARD":
             return state.concat(action.board)
-        case "ADD_BOARD_TO_MY_BOARDS":
-            return state.concat(action.board)
+        // case "ADD_BOARD_TO_MY_BOARDS":
+        //     return state.concat(action.board)
+        case "UPDATE_BOARD":
+            return state.map(board => board.id === action.board.id ? action.board : board) 
+        case "LIKE_BOARD":
+            return state.map(board => board.id === action.board.id ? action.board : board) 
         case "CLEAR_ALL_BOARDS":
             return initialState
+        case "DELETE_BOARD":
+            return state.filter(board => board.id === action.boardId ? false : true)
         default:
             return state   
     }
