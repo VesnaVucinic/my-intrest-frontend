4ccf8793a6ca87cc72d42a2dc910541ca965aa0d
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-01-13 22:18:51 +0000

    add search bar

--- src/components/AllBoards.js
+++ src/components/AllBoards.js
@@ -1,61 +1,73 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { likeBoard } from '../actions/myBoards'
+import { Card, Icon, Image, Label, Button } from 'semantic-ui-react'
 
-import { Card, Icon, Image, Label, Button, Segment  } from 'semantic-ui-react'
+class AllBoards extends React.Component {
+    state = {
+        search: ""
+    }
 
-const AllBoards = (props) => {
-    const allBoardsCards = props.boards.length > 0  
-    ? 
-        props.boards.map(board => (
-                <Card key={board.id} style={{ width: '15rem'}}>
-                    <Image src={board.attributes.image_url} wrapped ui={false}/>
-                    <Card.Content>
-                        <Card.Header>{board.attributes.name}</Card.Header>
-                        <Card.Description>
-                            Uploaded by {board.attributes.user.name}
-                        </Card.Description><br/>
-                        {/* <a>
-                            <Icon name='heart' color="red" size='large' onClick={() => props.likeBoard(board)}/>
-                            <strong>{board.attributes.likes}</strong>
-                        </a> */}
-                        
-                        {props.currentUser ?
-                            <Button as='div' labelPosition='right'>
-                                <Button icon  color='red' onClick={() => props.likeBoard(board)}>
-                                    <Icon name='heart' />
-                                    Like
-                                </Button>
-                                <Label as='a' basic color='red' basic pointing='left'>
-                                    <strong>{board.attributes.likes}</strong>
-                                </Label>
-                            </Button>
-                        : 
-                            <Button as='div' labelPosition='right'>
-                                <Button icon  color='red' >
-                                    <Icon name='heart' />
-                                    Liked
-                                </Button>
-                                <Label as='a'  color='red' basic pointing='left'>
-                                    <strong>{board.attributes.likes}</strong>
-                                </Label>
-                            </Button>
-                        }
-                    
-                    </Card.Content>
-                </Card>  
-            )) 
-    : 
-        null 
+    handleChange = e => {
+        this.setState({
+            search: e.target.value
+        })
+    }
 
-    return (
-        <Segment raised> 
-        <Card.Group style={{ margin: '15px', justifyContent: 'center'}}>
-            {allBoardsCards} 
-        </Card.Group>
-        </Segment>
-    )
+    render() {
+        const { search } = this.state;
+        const filteredBoards = this.props.boards.filter(board => {
+            return board.attributes.name.toLowerCase().indexOf(search.toLowerCase()) !== -1})
+        const allBoardsCards = this.props.boards.length > 0  
+            ? 
+            filteredBoards.map(board => (
+                        <Card  key={board.id} style={{ width: '15rem'}}>
+                            <Image src={board.attributes.image_url} wrapped ui={false}/>
+                            <Card.Content>
+                                <Card.Header>{board.attributes.name}</Card.Header>
+                                <Card.Description>
+                                    Uploaded by {board.attributes.user.name}
+                                </Card.Description><br/>
+                                {this.props.currentUser ?
+                                    <Button as='div' labelPosition='right'>
+                                        <Button icon  color='red' onClick={() => this.props.likeBoard(board)}>
+                                            <Icon name='heart' />
+                                            Like
+                                        </Button>
+                                        <Label as='a' basic color='red' basic pointing='left'>
+                                            <strong>{board.attributes.likes}</strong>
+                                        </Label>
+                                    </Button>
+                                : 
+                                    <Button as='div' labelPosition='right'>
+                                        <Button icon  color='red' >
+                                            <Icon name='heart' />
+                                            Liked
+                                        </Button>
+                                        <Label as='a'  color='red' basic pointing='left'>
+                                            <strong>{board.attributes.likes}</strong>
+                                        </Label>
+                                    </Button>
+                                }
+                            
+                            </Card.Content>
+                        </Card>  
+                    )) 
+            : 
+                null 
+
+            return (
+                <div>
+                    <input type='search' placeholder='Search board' onChange={this.handleChange}/>
+                    <Card.Group  style={{ margin: '15px', justifyContent: 'center'}}>
+                        {allBoardsCards} 
+                    </Card.Group>
+                </div>
+            )
+    }
+   
 }
+
 
 const mapStateToProps = state => {
     return {
